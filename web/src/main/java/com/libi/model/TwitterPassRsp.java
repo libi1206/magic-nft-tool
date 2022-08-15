b9038a14ccf42d676363c41142dfdf2f1dce21bb
李比 <843298330@qq.com>
2022-08-15 13:41:33 +0800

    fix: 🐛 fix

--- web/src/main/java/com/libi/model/TwitterPassRsp.java
+++ web/src/main/java/com/libi/model/TwitterPassRsp.java
@@ -6,10 +6,12 @@ import lombok.Data;
 @Data
 public class TwitterPassRsp {
     private String walletId;
+    private String machineCode;
 
     public static TwitterPassRsp of(TwitterPass twitterPass) {
         TwitterPassRsp rsp = new TwitterPassRsp();
         rsp.walletId = twitterPass.getWalletId();
+        rsp.machineCode = twitterPass.getMachineCode();
         return rsp;
     }
 }
